160790c057757d8e94ad40d030cb33358333a4e5
cyro <9770110+BlueCyro@users.noreply.github.com>
2025-07-15 03:32:04 -0700

    tweak(memory-file-unix)#: Deprecate ref counting mechanism in favor of relying on the programmer utilizing the 'deleteOnDispose' parameter for MemoryViewOptions/QueueOptions. This simplifies a lot of logic considerably and allows the file to be more reliably deleted by however a given platform (.NET runtime, mono, wine, etc.) implements 'FileOptions.DeleteOnClose'

--- src/Interprocess/Memory/AtomicFileCounter.cs
+++ src/Interprocess/Memory/AtomicFileCounter.cs
@@ -1,108 +1,105 @@
-using System.IO.MemoryMappedFiles;
-using System.Runtime.CompilerServices;
+// using System.IO.MemoryMappedFiles;
+// using System.Runtime.CompilerServices;
 
-namespace Cloudtoid.Interprocess.Memory.Unix;
+// namespace Cloudtoid.Interprocess.Memory.Unix;
 
-/// <summary>
-/// Uses interlocked operations on a memory-mapped file to count how many references to it exist.
-/// <para>
-/// Each instantiation of this class on the same file path will increment it's count by one,<br />
-/// and each disposal will decrease the count by one.
-/// </para>
-/// </summary>
-public sealed class AtomicFileCounter : IDisposable
-{
-    internal static readonly bool IsMonoUnderLinux = Type.GetType("Mono.Runtime") is not null
-        && Environment.OSVersion.Platform == PlatformID.Unix;
+// /// <summary>
+// /// Uses interlocked operations on a memory-mapped file to count how many references to it exist.
+// /// <para>
+// /// Each instantiation of this class on the same file path will increment it's count by one,<br />
+// /// and each disposal will decrease the count by one.
+// /// </para>
+// /// </summary>
+// public sealed class AtomicFileCounter : IDisposable
+// {
+//     private readonly string path;
+//     private readonly MemoryMappedFile file;
+//     private readonly MemoryMappedViewAccessor accessor;
+//     private readonly unsafe byte* ptr;
+//     private readonly Action<string>? cb;
+//     private readonly Mutex locker;
+//     private readonly FileStream? stream;
+//     private int disposed;
 
-    private readonly string path;
-    private readonly MemoryMappedFile file;
-    private readonly MemoryMappedViewAccessor accessor;
-    private readonly unsafe byte* ptr;
-    private readonly Action<string>? cb;
-    private readonly Mutex locker;
-    private readonly FileStream? stream;
-    private int disposed;
+//     /// <summary>
+//     /// Creates a new atomic file counter at the specified path.
+//     /// </summary>
+//     /// <param name="path">The path to the file counter.</param>
+//     /// <param name="callback">Callback to invoke when the atomic file counter reaches zero.</param>
+//     /// <param name="count">The count on the file at the time of instantiation.</param>
+// #pragma warning disable CA1021
+//     public unsafe AtomicFileCounter(string path, Action<string>? callback, out int count)
+// #pragma warning restore CA1021
+//     {
+//         locker = new(false, path.Replace('/', '_').Replace('\\', '_'));
+//         this.path = path;
+//         cb = callback;
 
-    /// <summary>
-    /// Creates a new atomic file counter at the specified path.
-    /// </summary>
-    /// <param name="path">The path to the file counter.</param>
-    /// <param name="callback">Callback to invoke when the atomic file counter reaches zero.</param>
-    /// <param name="count">The count on the file at the time of instantiation.</param>
-#pragma warning disable CA1021
-    public unsafe AtomicFileCounter(string path, Action<string>? callback, out int count)
-#pragma warning restore CA1021
-    {
-        locker = new(false, path.Replace('/', '_').Replace('\\', '_'));
-        this.path = path;
-        cb = callback;
+//         if (locker.WaitOne())
+//         {
+//             if (Util.IsMonoUnderLinux)
+//             {
+//                 file = MemoryMappedFile.CreateFromFile(
+//                     this.path,
+//                     FileMode.OpenOrCreate,
+//                     null,
+//                     4,
+//                     MemoryMappedFileAccess.ReadWrite);
+//             }
+//             else
+//             {
+//                 stream = new(this.path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+//                 file = MemoryMappedFile.CreateFromFile(
+//                     stream,
+//                     null,
+//                     4,
+//                     MemoryMappedFileAccess.ReadWrite,
+//                     HandleInheritability.None,
+//                     false);
+//             }
 
-        if (locker.WaitOne())
-        {
-            if (IsMonoUnderLinux)
-            {
-                file = MemoryMappedFile.CreateFromFile(
-                    this.path,
-                    FileMode.OpenOrCreate,
-                    null,
-                    4,
-                    MemoryMappedFileAccess.ReadWrite);
-            }
-            else
-            {
-                stream = new(this.path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
-                file = MemoryMappedFile.CreateFromFile(
-                    stream,
-                    null,
-                    4,
-                    MemoryMappedFileAccess.ReadWrite,
-                    HandleInheritability.None,
-                    false);
-            }
+//             accessor = file.CreateViewAccessor();
+//             accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref ptr);
+//             count = Interlocked.Increment(ref FileValue);
+//         }
+//         else
+//         {
+//             throw new Exception("Could not take mutex to atomic file, something is very wrong!!");
+//         }
 
-            accessor = file.CreateViewAccessor();
-            accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref ptr);
-            count = Interlocked.Increment(ref FileValue);
-        }
-        else
-        {
-            throw new Exception("Could not take mutex to atomic file, something is very wrong!!");
-        }
+//         locker.ReleaseMutex();
+//     }
 
-        locker.ReleaseMutex();
-    }
+//     /// <summary>
+//     /// How many references to this file currently exist.
+//     /// </summary>
+//     private unsafe ref int FileValue => ref Unsafe.As<byte, int>(ref Unsafe.AsRef<byte>(ptr));
 
-    /// <summary>
-    /// How many references to this file currently exist.
-    /// </summary>
-    private unsafe ref int FileValue => ref Unsafe.As<byte, int>(ref Unsafe.AsRef<byte>(ptr));
+//     /// <summary>
+//     /// Decrements the atomic counter by one and disposes of the memory map.
+//     /// </summary>
+//     public void Dispose()
+//     {
+//         if (Interlocked.CompareExchange(ref disposed, 1, 0) == 1)
+//             return;
 
-    /// <summary>
-    /// Decrements the atomic counter by one and disposes of the memory map.
-    /// </summary>
-    public void Dispose()
-    {
-        if (Interlocked.CompareExchange(ref disposed, 1, 0) == 1)
-            return;
+//         if (Interlocked.Decrement(ref FileValue) == 0 && locker.WaitOne())
+//         {
+//             try
+//             {
+//                 cb?.Invoke(path);
+//             }
+//             finally
+//             {
+//                 locker.ReleaseMutex();
+//             }
+//         }
 
-        if (Interlocked.Decrement(ref FileValue) == 0 && locker.WaitOne())
-        {
-            try
-            {
-                cb?.Invoke(path);
-            }
-            finally
-            {
-                locker.ReleaseMutex();
-            }
-        }
-
-        GC.SuppressFinalize(this);
-        accessor.SafeMemoryMappedViewHandle.ReleasePointer();
-        locker.Dispose();
-        accessor.Dispose();
-        file.Dispose();
-        stream?.Dispose();
-    }
-}
+//         GC.SuppressFinalize(this);
+//         accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+//         locker.Dispose();
+//         accessor.Dispose();
+//         file.Dispose();
+//         stream?.Dispose();
+//     }
+// }
--- src/Interprocess/Memory/MemoryFileUnix.cs
+++ src/Interprocess/Memory/MemoryFileUnix.cs
@@ -4,49 +4,43 @@ namespace Cloudtoid.Interprocess.Memory.Unix;
 
 internal sealed class MemoryFileUnix : IMemoryFile
 {
+    public MemoryMappedFile MappedFile { get; }
+
     private const string FileExtension = ".qu";
-    private const string LockExtension = ".qulock";
-    private const int BufferSize = 0x1000;
-    private static readonly string Folder = Path.Combine(".cloudtoid", "interprocess", "mmf");
     private readonly string file;
-    private readonly string lockFile;
-#pragma warning disable CA2213 // Disposable fields should be disposed
-    private readonly AtomicFileCounter counter; // This is disposed, the compiler is just silly.
-#pragma warning restore CA2213 // Disposable fields should be disposed
+
     private readonly ILogger<MemoryFileUnix> logger;
-    private readonly FileStream? stream;
+    private readonly FileStream stream;
 
     internal MemoryFileUnix(MemoryViewOptions options, ILoggerFactory loggerFactory)
     {
         logger = loggerFactory.CreateLogger<MemoryFileUnix>();
-        file = Path.Combine(options.Path, Folder);
-        Directory.CreateDirectory(file);
-        lockFile = Path.Combine(file, options.MemoryViewName + LockExtension);
-        file = Path.Combine(file, options.MemoryViewName + FileExtension);
-        counter = new AtomicFileCounter(lockFile, _ => ResetBackingFile(), out _);
+
+        long queueSize = options.GetActualStorageSize();
+        string folderPath = options.Path;
+        string viewName = options.MemoryViewName;
+
+        Directory.CreateDirectory(folderPath);
+        file = Path.Combine(folderPath, $"{viewName}{FileExtension}");
 
         try
         {
-            if (AtomicFileCounter.IsMonoUnderLinux)
-            {
-                MappedFile = MemoryMappedFile.CreateFromFile(
-                    file,
-                    FileMode.OpenOrCreate,
-                    null,
-                    BufferSize,
-                    MemoryMappedFileAccess.ReadWrite);
-            }
-            else
-            {
-                stream = new(file, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
-                MappedFile = MemoryMappedFile.CreateFromFile(
-                    stream,
-                    mapName: null,
-                    BufferSize,
-                    MemoryMappedFileAccess.ReadWrite,
-                    HandleInheritability.None,
-                    false);
-            }
+            stream = new(
+                file,
+                FileMode.OpenOrCreate,
+                FileAccess.ReadWrite,
+                FileShare.ReadWrite | FileShare.Delete,
+                Environment.SystemPageSize,
+                options.DestroyOnDispose ? FileOptions.DeleteOnClose : FileOptions.None);
+
+            stream.SetLength(queueSize); // Set length because mono sucks and will throw unless the capacity is just right.
+            MappedFile = MemoryMappedFile.CreateFromFile(
+                stream,
+                mapName: null,
+                options.GetActualStorageSize(),
+                MemoryMappedFileAccess.ReadWrite,
+                HandleInheritability.None,
+                false);
         }
         catch
         {
@@ -56,36 +50,17 @@ internal sealed class MemoryFileUnix : IMemoryFile
         }
     }
 
-    ~MemoryFileUnix() =>
-       Dispose(false);
-
-    public MemoryMappedFile MappedFile { get; }
-
     public void Dispose()
     {
-        Dispose(true);
+        MappedFile.Dispose();
         GC.SuppressFinalize(this);
-    }
-
-    private void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            MappedFile.Dispose();
-            counter.Dispose();
-            stream?.Dispose();
-        }
     }
 
     private void ResetBackingFile()
     {
-        // Deletes the backing file if it is not used by any other process
-        // if (IsFileInUse())
-        //     return;
-
-        if (!PathUtil.TryDeleteFile(file) || !PathUtil.TryDeleteFile(lockFile))
+        if (!PathUtil.TryDeleteFile(file))
             logger.FailedToDeleteSharedMemoryFile();
     }
 
-    // private bool IsFileInUse() => counter.Count > 0;
+    ~MemoryFileUnix() => Dispose();
 }
