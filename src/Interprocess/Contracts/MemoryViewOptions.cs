4d837ca5840e0a937e0642281ca9250b4263c1ad
cyro <9770110+BlueCyro@users.noreply.github.com>
2025-07-15 03:23:50 -0700

    feat(memory-options)#: Add "deleteOnDispose" parameter to constructors of MemoryViewOptions and QueueOptions to allow specifying if a given MemoryView should delete the file after it's disposed

--- src/Interprocess/Contracts/MemoryViewOptions.cs
+++ src/Interprocess/Contracts/MemoryViewOptions.cs
@@ -1,5 +1,4 @@
 ﻿using static Cloudtoid.Contract;
-using SysPath = System.IO.Path;
 
 namespace Cloudtoid.Interprocess;
 
@@ -12,7 +11,18 @@ public class MemoryViewOptions
     /// <param name="memoryViewName">The unique name of the memory view.</param>
     /// <param name="capacity">The maximum capacity of the memory view in bytes. This should be at least 16 bytes long and in the multiples of 8</param>
     public MemoryViewOptions(string memoryViewName, long capacity)
-        : this(memoryViewName, SysPath.GetTempPath(), capacity)
+        : this(memoryViewName, Util.MemoryFilePath, capacity)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MemoryViewOptions"/> class.
+    /// </summary>
+    /// <param name="memoryViewName">The unique name of the memory view.</param>
+    /// <param name="capacity">The maximum capacity of the memory view in bytes. This should be at least 16 bytes long and in the multiples of 8</param>
+    /// <param name="destroyOnDispose">Whether to destroy the backing file when this memory map is disposed.<br />NOTE: Only deletes the backing file if a file path is specified!</param>
+    public MemoryViewOptions(string memoryViewName, long capacity, bool destroyOnDispose)
+        : this(memoryViewName, Util.MemoryFilePath, capacity, destroyOnDispose)
     {
     }
 
@@ -35,6 +45,19 @@ public class MemoryViewOptions
     }
 
     /// <summary>
+    /// Initializes a new instance of the <see cref="QueueOptions"/> class.
+    /// </summary>
+    /// <param name="memoryViewName">The unique name of the queue.</param>
+    /// <param name="path">The path to the directory/folder in which the memory mapped and other files are stored in</param>
+    /// <param name="capacity">The maximum capacity of the queue in bytes. This should be at least 16 bytes long and in the multiples of 8</param>
+    /// <param name="destroyOnDispose">Whether to destroy the backing file when this memory map is disposed.<br />NOTE: Only deletes the backing file if a file path is specified!</param>
+    public unsafe MemoryViewOptions(
+        string memoryViewName,
+        string path,
+        long capacity,
+        bool destroyOnDispose) : this(memoryViewName, path, capacity) => DestroyOnDispose = destroyOnDispose;
+
+    /// <summary>
     /// Gets the unique name of the memory view.
     /// </summary>
     public string MemoryViewName { get; }
@@ -48,6 +71,12 @@ public class MemoryViewOptions
     /// Gets the size of the memory view in bytes.
     /// </summary>
     public long Capacity { get; }
+
+    /// <summary>
+    /// Gets whether to destroy the backing file once this process has disposed of the memory view.<br />
+    /// NOTE: Only deletes the backing file if a file path is specified!
+    /// </summary>
+    public bool DestroyOnDispose { get; }
 
     /// <summary>
     /// Gets the full size of this memory view. For raw memory view this is the same as Capacity.
--- src/Interprocess/Contracts/QueueOptions.cs
+++ src/Interprocess/Contracts/QueueOptions.cs
@@ -17,10 +17,33 @@ public sealed class QueueOptions : MemoryViewOptions
     /// Initializes a new instance of the <see cref="QueueOptions"/> class.
     /// </summary>
     /// <param name="queueName">The unique name of the queue.</param>
+    /// <param name="capacity">The maximum capacity of the queue in bytes. This should be at least 16 bytes long and in the multiples of 8</param>
+    /// <param name="deleteOnDispose">Whether to destroy the backing file when this memory map is disposed.<br />NOTE: Only deletes the backing file if a file path is specified!</param>
+    public QueueOptions(string queueName, long capacity, bool deleteOnDispose)
+        : base(queueName, capacity, deleteOnDispose)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QueueOptions"/> class.
+    /// </summary>
+    /// <param name="queueName">The unique name of the queue.</param>
     /// <param name="path">The path to the directory/folder in which the memory mapped and other files are stored in</param>
     /// <param name="capacity">The maximum capacity of the queue in bytes. This should be at least 16 bytes long and in the multiples of 8</param>
     public unsafe QueueOptions(string queueName, string path, long capacity)
         : base(queueName, path, capacity)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QueueOptions"/> class.
+    /// </summary>
+    /// <param name="queueName">The unique name of the queue.</param>
+    /// <param name="path">The path to the directory/folder in which the memory mapped and other files are stored in</param>
+    /// <param name="capacity">The maximum capacity of the queue in bytes. This should be at least 16 bytes long and in the multiples of 8</param>
+    /// <param name="deleteOnDispose">Whether to destroy the backing file when this memory map is disposed.<br />NOTE: Only deletes the backing file if a file path is specified!</param>
+    public unsafe QueueOptions(string queueName, string path, long capacity, bool deleteOnDispose)
+        : base(queueName, path, capacity, deleteOnDispose)
     {
     }
 
